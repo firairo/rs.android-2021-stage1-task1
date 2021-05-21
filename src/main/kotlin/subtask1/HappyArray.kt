e228af5a5843ede7d6af85557c8eb5f97507be0b
Lex Shi <firairo@gmail.com>
2021-05-21 19:14:45 +0300

    Initial commit. Tasks complited

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,23 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val sadArray = sadArray.toMutableList()
+        var i=1
+
+        while (i<=sadArray.size-2){
+            if(sadArray[i-1]+sadArray[i+1]<=sadArray[i]){
+                sadArray.removeAt(i)
+                i=1
+            }
+            i++
+        }
+
+        val good_array = sadArray.toIntArray()
+
+
+        return good_array
+
     }
 }
+
+
--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -5,6 +5,18 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        val input = bill.toMutableList()
+        val sum = input.sum()
+        val fairlySplit = "bon appetit"
+
+        val allergy =(sum-bill[k])/2
+        var anna: Int
+        if (b==allergy)
+            return fairlySplit
+
+        else
+            anna =b-allergy
+        return anna.toString()
+
     }
 }
